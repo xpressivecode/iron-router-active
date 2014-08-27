ee71da79153a1cacb5722fa5ef19c1501b158930
Luca Mussi <ing.luca.mussi@gmail.com>
2014-08-27 08:35:10 +0200

    adapted package.js for Meteor 0.9.0

--- package.js
+++ package.js
@@ -1,15 +1,19 @@
 Package.describe({
-  summary: "An iron-router module for quickly adding classnames to your active navigation elements."
+    summary: "An iron-router module for quickly adding classnames to your active navigation elements.",
+    version: "0.2.0",
+    name: "xpressivecode:iron-router-active",
+    git: "https://github.com/XpressiveCode/iron-router-active.git"
 });
 
 Package.on_use(function(api){
-  api.use([
-    'ui',
-    'iron-router'
-  ], 'client');
+    api.versionsFrom("METEOR@0.9.0");
+    api.use([
+        'ui',
+        'iron:router@0.9.1'
+    ], 'client');
 
-  api.add_files([
-    'LICENSE',
-    'lib/client/helpers.js'
-  ], 'client');
+    api.add_files([
+        'LICENSE',
+        'lib/client/helpers.js'
+    ], 'client');
 });
