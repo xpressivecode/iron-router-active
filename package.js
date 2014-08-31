53b2bfb48d72bd8a4422e89cd063f578ddb2a2c4
Joshua Skinner <jkyuusai@gmail.com>
2014-08-31 18:48:01 -0400

    Result of running mrt migrate-package on package
    https://hackpad.com/Migrating-Packages-zN0we9sIjkH

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "An iron-router module for quickly adding classnames to your active navigation elements."
+  summary: "An iron-router module for quickly adding classnames to your active navigation elements.",
+  version: "0.2.0",
+  git: "https://github.com/XpressiveCode/iron-router-active.git"
 });
 
 Package.on_use(function(api){
+  api.versionsFrom("METEOR@0.9.0");
   api.use([
     'ui',
     'iron-router'
